6c97d12d9103f8f55eac51bf66aeddf3c594c775
candid-ops <abdighafar223@gmail.com>
2025-09-18 15:06:29 -0700

    created the hero section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+import cartoon from '../../assets/cartoon.jpg'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <img src={cartoon} alt="Profile cartoon" />
+      <h1><span>I'M Abdulkarim,</span> Fullstack developer based in Kenya</h1>
+      <p>I'm a fullstack developer with 5 years experience</p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
