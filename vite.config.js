f3c6c1862ca58074f63bff418df514a1b733e3a6
candid-ops <abdighafar223@gmail.com>
2025-09-23 11:50:31 -0700

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-portfolio/",  // 👈 must match your repo name
+  base: "/react-portfolio/",  // 👈 Make sure this matches your repo name
+
+   server: {
+    host: '0.0.0.0',
+    port: 10000,  // Use the port expected by Render
+  }
 })
