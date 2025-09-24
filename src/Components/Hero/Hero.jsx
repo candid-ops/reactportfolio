75f38c9f90613462c0c98058ea7b1e11d4061b31
candid-ops <abdighafar223@gmail.com>
2025-09-23 18:17:48 -0700

    made the hero section responsive

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div id='home' className='hero'>
       <img src={cartoon} alt="Profile cartoon" />
       <h1><span>I'M Abdulkarim,</span> Fullstack developer based in Kenya</h1>
-      <p>I'm a fullstack developer with 5 years experience</p>
+      <p>I'm a fullstack developer with 5 years experience building dynamic, responsive, and scalable web applications. My expertise covers both frontend and backend development, creating seamless user experiences while ensuring performance and security.</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
         <div className="hero-resume">My resume</div>
